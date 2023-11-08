e9d708e22776e15e2033d104107212f3d1c349c2
Kevin Huang <89791540+KevinHuang2079@users.noreply.github.com>
2023-11-07 19:19:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ class StudentDatabase:
         self.mycursor.execute("CREATE TABLE IF NOT EXISTS Student(StudentId INTEGER PRIMARY KEY, FirstName TEXT, LastName TEXT, GPA REAL, Major TEXT, FacultyAdvisor TEXT, Address TEXT, City TEXT, State TEXT, ZipCode TEXT, MobilePhoneNumber TEXT, isDeleted INTEGER)")
         self.conn.commit()
 
+    """
+    Imports student data from a CSV file into a database.
 
+    Parameters:
+        csv_file (str): The path to a CSV file 
+
+    This function reads the specified CSV file, extracts student information, and inserts it into a database table named 'Student'.
+    The input CSV file should have columns for 'FirstName', 'LastName', 'GPA', 'Major', 'Address', 'City', 'State', 'ZipCode', and 'MobilePhoneNumber'.
+
+    Returns:
+        None
+    """
     def importData(self,csv_file):
         with open(csv_file, newline='') as f:
             reader = csv.DictReader(f)
@@ -33,12 +44,37 @@ class StudentDatabase:
                 self.mycursor.execute("INSERT INTO Student(FirstName, LastName, GPA, Major, FacultyAdvisor, Address, City, State, ZipCode, MobilePhoneNumber, isDeleted) VALUES(?,?,?,?,?,?,?,?,?,?,?)", values)
 
         self.conn.commit()
+    """
+    Retrieve and print data for active students from the database.
+
+    This function queries the 'Student' table for records where 'isDeleted' is 0.
+    Then it retrieves the data for these students and prints it to the console.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     def printData(self):
         self.mycursor.execute("SELECT* FROM Student WHERE isDeleted = 0")
         students = self.mycursor.fetchall()
         # Iterate through the selected rows and print each student's data
         for student in students:
             print(student)
+
+    """
+    Update student information in the 'Student' database table.
+
+    This function allows the user to update specific fields (Major, FacultyAdvisor, or MobilePhoneNumber) for a student by given studentID.
+    Checks if studentID exists, then prompts the user for the field and the new value.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     def updateStudent(self):
         while True:
             student_id = input("Enter Student ID for update, enter 'exit' to quit: ")
@@ -68,6 +104,19 @@ class StudentDatabase:
                 print("Invalid StudentId")
 
         self.conn.commit()
+
+    """
+    Soft-delete a student record in the 'Student' database table.
+
+    Allows the user to soft-delete a student by setting the 'isDeleted' field to 1.
+    First, prompt the user to enter the student's ID, and if it exists in the database, the 'isDeleted' field is updated to 1.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     def softDeleteStudent(self):
         while True:
             student_id = input("Enter Student ID for delete, enter 'exit' to quit: ").strip().lower()
@@ -87,6 +136,19 @@ class StudentDatabase:
                 continue
 
         self.conn.commit()
+
+    """
+    Search for students in the 'Student' database table based on specific criteria.
+
+    This function allows the user to search for students using search fields (GPA, City, State, or FacultyAdvisor).
+    The user can specify the search field to return students that matches the given field.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     def searchStudents(self):
         while True:
             user_input = input("Search by Major, GPA, City, State, or Advisor (or 'exit' to quit): ").strip().lower()
@@ -165,6 +227,19 @@ class StudentDatabase:
             else:
                 print("Invalid field.")
                 continue
+
+    """
+    Main menu for interacting with the 'Student' database.
+
+    Provides a user menu for database operations.
+    Choose from these options by entering the corresponding number.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     def mainMenu(self):
         validInputs = [1,2,3,4,5,6]
         mainInput = ""
@@ -177,15 +252,15 @@ class StudentDatabase:
                               "\nEnter 6: QUIT\n")
             try:
                 if (int(mainInput) == 1):
-                    db.importData()
+                    self.importData("students.csv")
                 elif (int(mainInput) == 2):
-                    db.printData()
+                    self.printData()
                 elif (int(mainInput) == 3):
-                    db.updateStudent()
+                    self.updateStudent()
                 elif (int(mainInput) == 4):
-                    db.softDeleteStudent()
+                    self.softDeleteStudent()
                 elif (int(mainInput) == 5):
-                    db.searchStudents()
+                    self.searchStudents()
                 elif (int(mainInput) == 6):
                     break
             except:
